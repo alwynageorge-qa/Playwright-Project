904adf1cc15064403995456194fac512f072e779
alwynageorge-qa <alwyna.george@akcelo.com>
2026-08-10 11:56:33 +1000

    chore: removing chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,15 +29,20 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    // Using HTTP/1.1 fallback to avoid failed chromium testcases
+    launchOptions: {
+    args: ['--disable-http2'], // Force HTTP/1.1
+    },
+    
     trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
-    {
+    /*{
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    },*/
 
     {
       name: 'firefox',
